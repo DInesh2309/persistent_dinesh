61e5dedc252bf02ac5fbc134d3eeba0645af1fcb
unknown <dinesh.pawar@omlp2p.com>
2022-05-03 19:53:03 +0530

    Updated database files

--- db/assignment.sql
+++ db/assignment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2022 at 02:59 PM
+-- Generation Time: May 03, 2022 at 04:06 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.23
 
@@ -41,10 +41,8 @@ CREATE TABLE `router_details` (
 --
 
 INSERT INTO `router_details` (`Id`, `Sapid`, `Hostname`, `Loopback`, `Macaddress`, `status`) VALUES
-(1, 'SAP-DINESH-MH-123B', 'DINESHM1234AAB', '255.254.254.255', 'D8-9C-67-AE-5B-23', 'A'),
-(2, 'SAP-DINESH-MH-123A', 'DINESHM1234AAA', '255.254.254.254', 'D8-9C-67-AE-5B-24', 'A'),
-(3, 'SAP-DINESH-MH-123C', 'DINESHM1234AAC', '255.254.254.255', 'D8-9C-67-AE-5B-23', 'A'),
-(4, 'SAP-DINESH-MH-123D', 'DINESHM1234AAD', '255.254.254.253', 'D8-9C-67-AE-5B-24', 'A');
+(1, 'SAP-DINESH-DP-1234', 'DINESDP1234ABC', '255.254.254.224', 'D8-9C-67-AE-5B-23', 'A'),
+(2, 'SAP-DINESH-DP-1224', 'DINESDP1234ABD', '255.254.254.234', 'D8-9C-67-AE-5B-24', 'A');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +62,7 @@ ALTER TABLE `router_details`
 -- AUTO_INCREMENT for table `router_details`
 --
 ALTER TABLE `router_details`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
